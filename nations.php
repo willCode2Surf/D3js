12563c9d74f8a91eed281c3a549c359add7acde4
Douglas Brown <austin.agile@gmail.com>
2012-08-21 08:41:43 -0500

    Initial commit

---
+++ nations.php
@@ -0,0 +1,9 @@
+<?php
+$Angola["name"] = "Angola";
+$Angola["region"] = "Sub-Saharan Africa";
+$Angola["income"] = array(array(1800,359.93), array(2009,5055.59));
+$Angola["population"] = array(array(1800,1567028), array(2008,12707546));
+$Angola["lifeExpectancy"] = array(array(1800,26.98), array(1940,26.98), array(2009,47.58));
+
+error_log(json_encode($Angola));
+?>
