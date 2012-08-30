45b5408c87f1604187cb00fe25d486e14742ca39
Douglas Brown <austin.agile@gmail.com>
2012-08-30 08:16:49 -0500

    x

--- nations.php
+++ nations.php
@@ -1,9 +1,8 @@
 <?php
-$Angola["name"] = "Angola";
-$Angola["region"] = "Sub-Saharan Africa";
-$Angola["income"] = array(array(1800,359.93), array(2009,5055.59));
-$Angola["population"] = array(array(1800,1567028), array(2008,12707546));
-$Angola["lifeExpectancy"] = array(array(1800,26.98), array(1940,26.98), array(2009,47.58));
+$Agent["name"] = "Beth";
+$Agent["team"] = "Lion";
+$Agent["att"] = array(array("1/1/2012",600), array("12/25/2012", 340));
+$Agent["fcr"] = array(array("1/1/2012",0.38), array("12/25/2012",0.40));
 
-error_log(json_encode($Angola));
+error_log(json_encode($Agent));
 ?>
